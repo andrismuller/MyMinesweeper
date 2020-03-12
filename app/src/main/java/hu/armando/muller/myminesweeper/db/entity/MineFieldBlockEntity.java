0b9945f70e0360e896f53803fdeb76ed8236cdec
andrismuller <muller.andras2@gmail.com>
2020-03-12 15:18:59 +0100

    Kind of initial commit. Setting up project.

---
+++ app/src/main/java/hu/armando/muller/myminesweeper/db/entity/MineFieldBlockEntity.java
@@ -0,0 +1,6 @@
+package hu.armando.muller.myminesweeper.db.entity;
+
+import hu.armando.muller.myminesweeper.model.MineFieldBlock;
+
+public class MineFieldBlockEntity implements MineFieldBlock {
+}
---
+++ app/src/main/java/hu/armando/muller/myminesweeper/db/entity/MineFieldEntity.java
@@ -0,0 +1,6 @@
+package hu.armando.muller.myminesweeper.db.entity;
+
+import hu.armando.muller.myminesweeper.model.MineField;
+
+public class MineFieldEntity implements MineField {
+}
